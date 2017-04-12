710a25fe217f1eea65b824f3cc76cac22e82de53
Danuta <jolanta.zakrzewska@orange.pl>
2017-04-12 18:36:34 +0200

    debug settings & splash screen

---
+++ app/src/main/java/com/odys/startrekgame/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.odys.startrekgame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        thread.start();
+    }
+}

3116a533b6174a110bf25b8e5968d8684af6afd5
Danuta <jolanta.zakrzewska@orange.pl>
2017-04-17 22:37:46 +0200

    lazy fixes

--- app/src/main/java/com/odys/startrekgame/ResultActivity.java
+++ app/src/main/java/com/odys/startrekgame/ResultActivity.java
@@ -68,7 +68,7 @@ public class ResultActivity extends AppCompatActivity {
 
 
     public void tryAgain(View view) {
-        startActivity(new Intent(getApplicationContext(), StartActivity.class));
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
         finish();
     }
 
